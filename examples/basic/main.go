c23315d214bc5c8653eaa393d9cfc5dc83dce338
Cem <cembkoztekin@gmail.com>
2023-05-27 19:48:37 +0300

    integrate tcmd client

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
+	"github.com/0bl/tcmd"
 	"github.com/gempir/go-twitch-irc/v4"
 )
 
@@ -15,6 +17,11 @@ func must(err error) {
 
 func main() {
 	c := twitch.NewAnonymousClient()
+
+	tcmd, err := tcmd.NewClient(c, tcmd.WithPrefix("?"))
+	must(err)
+
+	fmt.Println(tcmd.GetPrefix())
 
 	c.OnPrivateMessage(func(msg twitch.PrivateMessage) {
 		if strings.Contains(strings.ToLower(msg.Message), "ping") {
